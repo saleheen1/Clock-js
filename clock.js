762dcc01a14230040849d0c1e1dae6632f7a77d4
saleheen1 <smsaleheen18@gmail.com>
2020-01-26 22:22:55 +0600

    js functionality added

--- clock.js
+++ clock.js
@@ -1,1 +1,24 @@
-const second = document.querySelector('.second-hand');
+const second = document.querySelector('.second');
+const minutes = document.querySelector('.min');
+const hour = document.querySelector('.hour');
+const hand = document.querySelector('.hand');
+
+setInterval(() => {
+    const date = new Date();
+    // console.log(date);
+    const sec = date.getSeconds();
+    const min = date.getMinutes();
+    const hr = date.getHours();
+
+    const minDeg = ((min/60)*360 )+90;
+    const secDeg = ((sec/60)*360) +90;
+    const hrDeg = ((hr/12)*360) +90;
+
+    if(sec===0 || sec ===1){
+        second.style.transition ='0s';
+    }
+    second.style.transform = `rotate(${secDeg}deg)`;
+    minutes.style.transform = `rotate(${minDeg}deg)`;
+    hour.style.transform = `rotate(${hrDeg}deg)`;
+    
+}, 1000);
